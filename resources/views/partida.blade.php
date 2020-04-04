ccd5e634874213956d9827c363f350aace4249c9
ClaroQuintans <luisclaroq@gmail.com>
2020-04-04 09:38:17 +0200

    ficheros originales

---
+++ resources/views/partida.blade.php
@@ -0,0 +1,19 @@
+<!DOCTYPE html>
+<html lang="{{ str_replace('_', '-', app()->getLocale()) }}">
+
+    <body>
+    <div>
+    <img src="{{URL::asset('/img/cartas/bastos_1.jpg')}}">
+    </div>
+
+    <div>
+    </div>
+
+    <div>
+    </div>
+
+    <div>
+    </div>
+
+    </body>
+</html>
